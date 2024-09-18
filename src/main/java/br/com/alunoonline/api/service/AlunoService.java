16e9af1a36a5643cc49399c72f16729cb54b6597
LeoFhin <leonardo.santos7507@gmail.com>
2024-09-18 17:18:23 -0300

    preparação do contrroler, repository, model e service de professor

--- src/main/java/br/com/alunoonline/api/service/AlunoService.java
+++ src/main/java/br/com/alunoonline/api/service/AlunoService.java
@@ -3,7 +3,9 @@ package br.com.alunoonline.api.service;
 import br.com.alunoonline.api.model.Aluno;
 import br.com.alunoonline.api.repository.AlunoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
@@ -28,6 +30,32 @@ public class AlunoService {
 
     public void deletarAlunoPorId(Long id){
         alunoRepository.deleteById(id);
+    }
+
+    public void atualizarAlunoPorId(Long id, Aluno aluno){
+
+        // Primeiro: Ver se o aluno existe no BD
+        Optional<Aluno> alunoDoBancoDeDados = buscarAlunoPorId(id);
+        // E se não existir?
+        if (alunoDoBancoDeDados.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Aluno não encontrado no banco de dados!");
+        }
+            //Se chegar aqui, significa que existe aluno, então vou armazena-lo em uma variavel
+
+        Aluno alunoEditado = alunoDoBancoDeDados.get();
+
+        // Com esse aluno editado de cima, faço...
+        // faço os sets necessarios para atualizar os atributos deles.
+
+        alunoEditado.setNome(aluno.getNome());
+        alunoEditado.setCpf(aluno.getCpf());
+        alunoEditado.setEmail(aluno.getEmail());
+
+        //Com o aluno totalmente editado acima
+        //eu devolvo ele atualizado para o banco de dados
+
+        alunoRepository.save(alunoEditado);
+
     }
 
 
